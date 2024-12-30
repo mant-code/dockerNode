e1cea5097962121d5150d0ca20f93fa7455b496c
mant-code <ashishlawaju143@gmail.com>
2024-12-30 15:56:20 +0545

    node and mongo

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const app = express()
 
 app.get("/",(req,res)=> res.json("hello ashish !"))
 
+
+const mongoURI = "mongodb://mongodb:27017"
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+    .then(() => {
+      console.log('MongoDB connected successfully!');
+    })
+    .catch((err) => {
+      console.error('Error connecting to MongoDB:', err);
+    });
 app.listen(4000,()=>{
     console.log("server is starting!")
 })
